901e60ace4b8b086289a754ca45831188de5bed6
SmoothBot <smoothbot123@gmail.com>
2023-05-26 10:22:37 +1000

    Updating arkiver urls

--- src/lib/datasource/camelotFarm.ts
+++ src/lib/datasource/camelotFarm.ts
@@ -37,7 +37,7 @@ export class CamelotFarmDataSource implements DataSource<CamelotFarmSnapshot> {
 		if (this.config.pools[0] !== USDCWETH)
 			throw new Error('Invalid pools for Camelot Farm - Only ETHUSDC is supported currently')
 
-		const url = 'https://data.arkiver.net/s_battenally/cpmm_v2/graphql'
+		const url = 'https://data.staging.arkiver.net/s_battenally/cpmm_v2/graphql'
         this.client = new GraphQLClient(url, { headers: {} })
 	}
 
